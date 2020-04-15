01bda590ab13c958db31d327951423a4221f78ef
jenny.kr <jenny.it.kr@gmail.com>
2020-04-15 22:34:09 +0900

    Add Kakao map

--- map/static/js/index.js
+++ map/static/js/index.js
@@ -1,6 +1,7 @@
-const title = document.querySelector("h1");
+const container = document.getElementById("map");
+const options = {
+  center: new kakao.maps.LatLng(33.450701, 126.570667),
+  level: 3,
+};
 
-title.addEventListener(
-  "click",
-  (event) => (event.target.style.color = "blue")
-);
+const map = new kakao.maps.Map(container, options);
